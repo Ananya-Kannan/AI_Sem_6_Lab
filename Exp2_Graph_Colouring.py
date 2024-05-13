8ec92fdfd1db19080a7b6277e9379de2a7d858d5
Ananya Kannan <91649989+Ananya-Kannan@users.noreply.github.com>
2024-05-13 16:16:45 +0530

    Update Exp2_Graph_Colouring.py

--- Exp2_Graph_Colouring.py
+++ Exp2_Graph_Colouring.py
@@ -1,46 +1,16 @@
-def addEdge(adj, v, w):
-    adj[v].append(w) 
-    adj[w].append(v) 
-    return adj 
- 
-def greedyColoring(adj, V):
-    result = [-1] * V 
-    result[0] = 0;
-    available = [False] * V
-    for u in range(1, V):
-        for i in adj[u]:
-            if (result[i] != -1):
-                available[result[i]] = True 
-        cr = 0
-        while cr < V:
-            if (available[cr] == False):
-                break
-            cr += 1
-        result[u] = cr
-        for i in adj[u]:
-            if (result[i] != -1):
-                available[result[i]] = False
-    for u in range(V):
-        print("Vertex", u, " ---> Color", result[u]) 
- 
+from constraint import Problem
 
-  
-g1 = [[] for i in range(5)] 
-g1 = addEdge(g1, 0, 1) 
-g1 = addEdge(g1, 0, 2) 
-g1 = addEdge(g1, 1, 2) 
-g1 = addEdge(g1, 1, 3) 
-g1 = addEdge(g1, 2, 3) 
-g1 = addEdge(g1, 3, 4) 
-print("Coloring of graph 1 ") 
-greedyColoring(g1, 5) 
- 
-g2 = [[] for i in range(5)] 
-g2 = addEdge(g2, 0, 1) 
-g2 = addEdge(g2, 0, 2) 
-g2 = addEdge(g2, 1, 2) 
-g2 = addEdge(g2, 1, 4) 
-g2 = addEdge(g2, 2, 4) 
-g2 = addEdge(g2, 4, 3) 
-print("\nColoring of graph 2") 
-greedyColoring(g2, 5)
+problem = Problem()
+
+num_regions = int(input("Enter the number of regions: "))
+colors = input("Enter the colors separated by space: ").split()
+
+for region in range(1, num_regions + 1):
+    problem.addVariable(region, colors)
+
+for _ in range(int(input("Enter the number of neighbouring region pairs: "))):
+    region1, region2 = input("Enter two neighbouring regions separated by space: ").split()
+    problem.addConstraint(lambda color1, color2: color1 != color2, (int(region1), int(region2)))
+
+solution = problem.getSolutions()[0]
+print(solution)
